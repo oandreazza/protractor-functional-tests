b9175f18132f6adba648b120bae1537f8215b082
Mauricio Sganderla <mauricio.webdev@gmail.com>
2017-12-10 21:49:13 -0200

    trying with reports on circleci

--- config.js
+++ config.js
@@ -7,5 +7,25 @@ exports.config = {
     chromeOptions: {
      args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
    }
+ },
+
+  framework: 'jasmine',
+  onPrepare: function() {
+    var jasmineReporters = require('jasmine-reporters');
+    var junitReporter = new jasmineReporters.JUnitXmlReporter({
+
+      // setup the output path for the junit reports
+      savePath: '$CIRCLE_TEST_REPORTS/output/',
+
+      // conslidate all true:
+      //   output/junitresults.xml
+      //
+      // conslidate all set to false:
+      //   output/junitresults-example1.xml
+      //   output/junitresults-example2.xml
+      consolidateAll: false
+
+    });
+    jasmine.getEnv().addReporter(junitReporter);
   }
 };
