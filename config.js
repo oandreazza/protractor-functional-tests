53b61602bc48d429632ff78d03e14e492a8784b8
Mauricio Sganderla <mauricio.webdev@gmail.com>
2017-12-10 21:07:03 -0200

    starting with circle ci

--- config.js
+++ config.js
@@ -3,6 +3,9 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['specs/*.js'],
   capabilities: {
-    'browserName': 'chrome',
+    browserName: 'chrome',
+    chromeOptions: {
+     args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+   }
   }
 };
